5c1f6156d528162dadff76854346f6f206a3beec
Ryan <jesselockard99@gmail.com>
2021-03-21 02:27:13 -0400

    updated documentation for render.js

--- src/html/components/render.js
+++ src/html/components/render.js
@@ -1,7 +1,12 @@
 import { useCallback, useEffect, useState } from "https://unpkg.com/preact/hooks/dist/hooks.module.js?module"
 
 /**
- * 
+ * `<Render>` is a component which listens for an NUI `message` from a Client-script.
+ *
+ * This message should contain a `show` property which is either `true` or `false`.
+ *
+ * * `true` - Will display ("render") the component's children.
+ * * `false` - Will hide the component's children, completely removing them from the webpage's DOM.
  */
 const Render = ({ children }) => {
   const [isRendering, setIsRendering] = useState(false)
